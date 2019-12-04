45f7a9ce13027ed2e2fbe5a48db4c6e326dd1417
taohr <22227124@qq.com>
2019-12-04 17:00:27 +0800

    project inited

---
+++ webpack.common.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Vivian Flower',
+      inject: true,
+      template: path.resolve(__dirname, './src/index.html'),
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    alias: {
+      'vue': 'vue/dist/vue.js',
+    }
+  }
+};
